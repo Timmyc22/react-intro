090f7174b42073f01759f8dd8461fee361c864b4
Timothy Coleman <Timbo97@yahoo.com>
2023-12-28 11:16:25 -0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './vite.svg'; // Fixed the path for viteLogo
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState(''); // Added state for text input
+  const [list, setList] = useState(["ready", "set", "GO"]); // Added state for todo list
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setList((prevList) => [...prevList, text]);
+    setText('');
+  };
 
   return (
     <>
@@ -21,22 +33,28 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-  React is so cool!
-        </p>
+        <p>React is so cool!</p>
         <h1>Hello React!</h1>
-      <p>Learning React is exciting!</p>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+        <p>Learning React is exciting!</p>
+        <ul>
+          {list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            value={text}
+            onChange={onChange}
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
